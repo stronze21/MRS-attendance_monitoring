1f0786eead3fbebf46695f2492ff1352074ee34b
stronze21 <57517246+stronze21@users.noreply.github.com>
2023-10-03 10:53:20 +0800

    10-03-2023

--- app/Livewire/AttendanceInterface.php
+++ app/Livewire/AttendanceInterface.php
@@ -28,23 +28,23 @@ class AttendanceInterface extends Component
 
     public function send_message($number, $message)
     {
-        // $ch = curl_init();
-        // $parameters = array(
-        //     'apikey' => env('SMS_API_KEY'), //Your API KEY
-        //     'number' => $number,
-        //     'message' => $message,
-        //     'sendername' => 'SEMAPHORE'
-        // );
-        // curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
-        // curl_setopt($ch, CURLOPT_POST, 1);
+        $ch = curl_init();
+        $parameters = array(
+            'apikey' => env('SMS_API_KEY'), //Your API KEY
+            'number' => $number,
+            'message' => $message,
+            'sendername' => 'MRSDCS'
+        );
+        curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
+        curl_setopt($ch, CURLOPT_POST, 1);
 
-        // //Send the parameters set above with the request
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+        //Send the parameters set above with the request
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
 
-        // // Receive response from server
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // $output = curl_exec($ch);
-        // curl_close($ch);
+        // Receive response from server
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
         return;
     }
 
